4c6847a2f35c28db2c4ef89be7f742735be17e3e
Marco Antônio <mjota51@gmail.com>
2018-01-15 23:15:11 -0200

    Setup commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,40 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { RoutingModule } from './app.router';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { WishlistComponent } from './components/wishlist/wishlist.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { AppListComponent } from './components/app-list/app-list.component';
-import { AppCardComponent } from './components/app-card/app-card.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { AppPageComponent } from './pages/app-page/app-page.component';
-import { SinglePageComponent } from './pages/single-page/single-page.component';
-import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    NavigationComponent,
-    SearchbarComponent,
-    WishlistComponent,
-    HeroComponent,
-    AppListComponent,
-    AppCardComponent,
-    FooterComponent,
-    HomePageComponent,
-    AppPageComponent,
-    SinglePageComponent,
-    Error404PageComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
